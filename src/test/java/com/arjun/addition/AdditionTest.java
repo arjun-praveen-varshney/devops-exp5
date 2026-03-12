85f64f6c15d9b1d0c32e49f1d4a8801adad3ef8c
Arjun Praveen Varshney <arjun.varshney1423@gmail.com>
2026-03-12 12:31:13 +0530

    testng 3 config files changed

--- src/test/java/com/arjun/addition/AdditionTest.java
+++ src/test/java/com/arjun/addition/AdditionTest.java
@@ -1,6 +1,6 @@
 package com.arjun.addition;
 
-import org.junit.Test;
+import org.testng.annotations.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
